5cee2534e82002869660aad7d290f74600a3687a
Md Nahid Hassan <princenahidcse1@gmail.com>
2025-01-16 17:52:43 +0600

    requirment added

--- job_project/settings.py
+++ job_project/settings.py
@@ -144,11 +144,11 @@ WSGI_APPLICATION = 'job_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'job_project', 
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'NAME': env('DB_NAME'), 
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'), 
+        'PORT': env('DB_PORT'),
     }
 }
 
